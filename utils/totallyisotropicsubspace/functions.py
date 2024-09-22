c9c44576044b73bebf823da228ba5b4949f220c7
hunglt <thaihungk56neu@gmail.com>
2024-09-22 20:44:46 +0200

    Update diagonalization algos

--- utils/totallyisotropicsubspace/functions.py
+++ utils/totallyisotropicsubspace/functions.py
@@ -220,3 +220,194 @@ def brute_force_search_isotropic_matrices(A: Matrix, F: FiniteField, m: int) -> 
 
     return valid_matrices  # Return the list of valid matrices
 
+# Check if a matrix A is an alternating matrix
+def check_alternating_matrix(A: Matrix, F: FiniteField) -> bool:
+    """
+    Check if a matrix is an alternating matrix
+
+    Args:
+        A (Matrix): Any matrix
+        F (FiniteField): Finite field
+
+    Returns:
+        bool: True/False
+    """
+    n_rows = A.nrows()
+    n_cols = A.ncols()
+    
+    # The matrix must be square
+    if n_rows != n_cols:
+        return False
+    
+    # Check if all elements in the diagonal is zero or not:
+    for i in range(n_rows):
+        if A[i,i] != 0:
+            return False
+        
+    # Check if the matrix is skew-symmetric, i.e. A = - A.transpose()
+    for i in range(n_rows):
+        for j in range(i+1,n_cols):
+            if A[i,j] != A[j,i]:
+                return False
+            
+    return True
+
+# Diagonalize top-left 2x2 block
+def diagonalize_2x2_alternating_matrix(A: Matrix, F: FiniteField) -> Matrix:
+    """
+    Diagonalize the top-left 2x2 block
+
+    Args:
+        A (Matrix): Matrix
+        F (FiniteFiel): Finite field
+
+    Returns:
+        Matrix: Diagonalized-top-left-block matrix
+    """
+    # Check if the input matrix is a full-rank alternating matrix
+    if check_alternating_matrix(A,F) == False:
+        print("A is not a full-rank alternating matrix")
+        return None    
+    
+    if A.is_zero():
+        return A
+    
+    # Number of rows and columns
+    n_rows = A.nrows()
+    n_cols = A.ncols()
+    
+    # Check of A[0,1] != 0
+    if A[0,1] != 0:   
+        # Create an identity matrix of size (n_rows - 2) x (n_cols - 2)
+        I = matrix.identity(F,n_rows -2)
+        
+        # Create top-left matrix
+        L_1 = matrix(F,[[1,0],[0,A[0,1].inverse()]])
+        
+        # Create top-right matrix
+        L_2 = zero_matrix(F,2,n_cols-2)
+        
+        # Create bottom-left matrix
+        list_values = []
+        for i in range(2,n_rows):
+            list_values.append(A[0,i]*A[0,1].inverse())
+            
+        L_3 = matrix(F,[[0]*(n_rows - 2),list_values])
+        L_3 = L_3.transpose()
+        
+        # Construct L
+        L = block_matrix(F,[[L_1,L_2],[L_3,I]],subdivise = False)
+        
+        return L
+    
+    if A[0,1] == 0:
+        print("Switching ... \n")
+        # Search for any A[0,i] != 0, it must exist at least one since A is a full-rank matrix
+        for i in range(0,n_cols):
+            if A[0,i] != 0:
+                switch_index = i # This is the switching index
+                break
+
+        # Create a column switching matrix of size similar to A
+        I = matrix.identity(F,n_rows)
+        
+        # Swap columns i and j
+        I.swap_columns(i, 1)
+        
+        # Return the matrix A_new that has column 2 and i+1 switched
+        A_switched = I*A*I.transpose()
+        
+        # Create bottom-righ matrix i.e. an identity matrix of size (n_rows - 2) x (n_cols - 2)
+        K = matrix.identity(F,n_rows -2)
+        
+        # Create top-left matrix
+        L_1 = matrix(F,[[1,0],[0,A_switched[0,1].inverse()]])
+        
+        # Create top-right matrix
+        L_2 = zero_matrix(F,2,n_cols-2)
+        
+        # Create bottom-left matrix
+        list_values = []
+        for i in range(2,n_rows):
+            list_values.append(A_switched[0,i]*A_switched[0,1].inverse())
+            
+        L_3 = matrix(F,[[0]*(n_rows - 2),list_values])
+        L_3 = L_3.transpose()
+        
+        # Construct L
+        L = block_matrix(F,[[L_1,L_2],[L_3,K]],subdivise = False)
+        
+        return L*I # Return switched L
+    
+# Diagonalize an alternating matrix
+def diagonalize_full_alternating_matrix(A: Matrix, F: FiniteField) -> Matrix:
+    """
+    Return a matrix L such that L is invertible and L.transpose()*A*L = 
+    
+    [0 1 0 0]
+    [1 0 0 0]
+    [0 0 0 1]
+    [0 0 1 0]
+
+    Args:
+        A (Matrix): an alternating matrix i.e. A is skew-symmetric and has zeros in its diagonal
+        F (FiniteField): Finite Field
+
+    Returns:
+        Matrix: Matrix L
+    """
+    
+    # Rows and Columns
+    n_rows = A.nrows()
+    n_cols = A.ncols()
+    # A_original = A.copy()
+    
+    # Diagonalizing block by block
+    list_i_A_L = []
+    
+    for i in tqdm(range(n_rows),ncols = 100,desc="Diagonalizing block by block ..."):
+        if i == 0:
+            # Get the diagonalizing matrix of the first block
+            L = diagonalize_2x2_alternating_matrix(A,F)
+            
+            # Get new A
+            A = L*A*L.transpose()
+            # print("\n",A,"\n")
+            # Extract a submatrix of size (n_rows - 1) x (n_cols - 1)
+            A = A.submatrix(1, 1, A.nrows()-1, A.ncols()-1)
+            # print("\n",A,"\n")
+            # Append to the list
+            list_i_A_L.append((i,A,L))
+            
+        if i != 0:
+            A = list_i_A_L[i-1][1]
+            print(" new A is  \n",A,"\n")
+
+            # Get the diagonalizing matrix of the first block as bottomright corner
+            L = diagonalize_2x2_alternating_matrix(A,F)
+            
+            # Get new A
+            A = L*A*L.transpose()
+            # print("\n After transform \n",A,"\n")
+            
+            # Get the identity matrix of size i * i as topleft corner
+            I = matrix.identity(F,i)
+
+            # Construct the block matrix
+            L = block_diagonal_matrix(I,L)
+
+            # Extract a submatrix of size (n_rows - 1) x (n_cols - 1)
+            A = A.submatrix(1, 1, A.nrows()-1, A.ncols()-1)
+                        
+            # Append to the list
+            list_i_A_L.append((i,A,L))
+                        
+    # Initialize the result_L as an identity matrix of the appropriate size
+    result_L = matrix.identity(F, n_rows)   
+    
+    # Iterate through list_i_A_L and multiply all matrices L
+    for _, _, L in list_i_A_L[::-1]:
+        result_L *= L  # Multiply result by each matrix L
+
+    # The result now contains the product of all matrices in list_L
+    return result_L
--- utils/totallyisotropicsubspace/main.py
+++ utils/totallyisotropicsubspace/main.py
@@ -1,16 +1,24 @@
 from functions import *
 
-F = GF(2,'x')
-n = 8
-A = create_anti_identity_matrix(F,n//2)
-print(A)
+F = GF(4,'x')
+n = 12
+# A = create_anti_identity_matrix(F,n//2)
+A = random_matrix(F,n,n)
+A = A + A.transpose()
+print("\n A = \n", A, "\n of rank ",A.rank())
 
-# Set identity matrix of size n * n
-I = matrix.identity(F,n)
+# # Set identity matrix of size n * n
+# I = matrix.identity(F,n)
 
-# Brute force search isotropic matrices size m * n
+# # Brute force search isotropic matrices size m * n
 
-list_iso_matrices = brute_force_search_isotropic_matrices(A,F,n//2)
+# list_iso_matrices = brute_force_search_isotropic_matrices(A,F,n//2)
 
-number_iso_matrices = len(list_iso_matrices)
-print(number_iso_matrices)
+# number_iso_matrices = len(list_iso_matrices)
+# print(number_iso_matrices)
+
+
+L = diagonalize_full_alternating_matrix(A,F)
+print("L = \n ", L, " of rank ",L.rank())
+if L != None:
+    print("Check \n", L*A*L.transpose())
